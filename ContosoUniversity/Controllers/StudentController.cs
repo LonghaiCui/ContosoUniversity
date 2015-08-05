5cf64e1c80eb054e9669882f4525c3390089568d
longhai <lcyvb@mail.missouri.edu>
2015-08-05 18:53:13 +0000

    Add Paging Functionality to the Index Method

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -8,7 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
-
+using PagedList;
 namespace ContosoUniversity.Controllers
 {
     public class StudentController : Controller
@@ -16,10 +16,18 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index(string sortOrder, string searchString)
+        public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
         {
+            ViewBag.CurentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder)? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
 
             var students = from s in db.Students
                            select s;
@@ -43,8 +51,9 @@ namespace ContosoUniversity.Controllers
                     students = students.OrderBy( s => s.LastName);
                     break;
             }
-
-            return View(students.ToList());
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Student/Details/5
