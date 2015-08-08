fe115f1e491562fba52fdddb01604cf1792e8bcf
longhai <lcyvb@mail.missouri.edu>
2015-08-08 20:33:07 +0000

    Adding Explicit Loading for enrollment(collection) and student(refference). Also Fix the Instructor date display

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -23,7 +23,7 @@ namespace ContosoUniversity.Models
         public string FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString="0:yyyy-MM-dd",ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
 
         [Display(Name="Full Name")]
